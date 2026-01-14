99c18a30a647c75479ec5c3fda56f485f35f3a35
Davi Bonetto <davi.bonetto100@gmail.com>
2026-01-14 13:51:31 -0300

    refactor: remove status from footer and increase page gap to 40

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,15 +14,6 @@ export function Footer() {
               The gap between where you are and where you want to be is just code.
             </p>
           </div>
-           <div className="flex items-center gap-2">
-            <span className="relative flex h-2 w-2">
-              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75"></span>
-              <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
-            </span>
-            <span className="text-[10px] font-mono uppercase tracking-widest text-[#a1a1aa]">
-              All Systems Operational
-            </span>
-          </div>
         </div>
 
         {/* Social Section - Interactive Blocks */}
